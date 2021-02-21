05613bab44d01d2d98a7b0bbab48b1f319e845e1
hengooo <72793395+hengooo@users.noreply.github.com>
2021-02-21 18:15:10 +0800

    practice upgrade

--- 课练（指针赋值与类型转换）.cpp
+++ 课练（指针赋值与类型转换）.cpp
@@ -2,7 +2,11 @@
 int main(){
 	int num=1024;
 	int *pi =&num;//指针char*的意思是给我个字符串我要修改它但理论上是无法修改的 
-	const char*="Fish";//所以可以在前面加一个const变为const char*意思是给我个字符串，我要读取它。 
+	//(const char*类型不能用于初始化char*类型的实体的解决方法)
+	const char*pt="Fish";//方法1；可以在前面加一个const变为const char*意思是给我个字符串，我要读取它。 
+	char *pp=(char *)"Fish";//方法2：强制转化为char*类型 
+	char pm[]="Fish";
+	char *pm1=pm;//方法3：对变量先进行字符数组声明。 
 	char *ps = "Fish";//理论上是不能直接将字符或者字符串直接赋给指针的 
 	void *pv;//空白指针，其他任何类型指针均可以指向空白类型指针。 
 	
